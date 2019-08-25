5dc420afa098d5a15922a6f7c3f5098ed3fb12c3
codeandomx <admin@codeando.org>
2019-08-25 19:32:35 +0000

    Se corrige sección content del layout principal.

--- src/comisaria/resources/views/layouts/main_layout.blade.php
+++ src/comisaria/resources/views/layouts/main_layout.blade.php
@@ -42,82 +42,80 @@
 
 		<!-- Sidebar -->
 		<div class="site-overlay"></div>
-			<div class="site-sidebar">
-				<div class="custom-scroll custom-scroll-dark">
-					<ul class="sidebar-menu">
-						<li class="menu-title">Juridico</li>
-						<li class="with-sub">
-							<a href="#" class="waves-effect  waves-light">
-								<span class="s-caret"><i class="fa fa-angle-down"></i></span>
-								<!--
-									<span class="tag tag-purple">3</span>
-								-->
-								<span class="s-icon"><i class="ti-book"></i></span>
-								<span class="s-text">Departamento 1</span>
-							</a>
-							<ul>
-								<li><a href="index.html">Formulario 1</a></li>
-								<li><a href="index2.html">Formulario 2</a></li>
-								<li><a href="index3.html">Formulario 3</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
+		<div class="site-sidebar">
+			<div class="custom-scroll custom-scroll-dark">
+				<ul class="sidebar-menu">
+					<li class="menu-title">Juridico</li>
+					<li class="with-sub">
+						<a href="#" class="waves-effect  waves-light">
+							<span class="s-caret"><i class="fa fa-angle-down"></i></span>
+							<!--
+								<span class="tag tag-purple">3</span>
+							-->
+							<span class="s-icon"><i class="ti-book"></i></span>
+							<span class="s-text">Departamento 1</span>
+						</a>
+						<ul>
+							<li><a href="index.html">Formulario 1</a></li>
+							<li><a href="index2.html">Formulario 2</a></li>
+							<li><a href="index3.html">Formulario 3</a></li>
+						</ul>
+					</li>
+				</ul>
 			</div>
 		</div>
 
         <!-- Header -->
         <div class="site-header">
-				<nav class="navbar navbar-dark">
-					<div class="navbar-left">
-						<a class="navbar-brand" href="/">
-							<div class="logo"></div>
-						</a>
-						<div class="toggle-button dark float-xs-right hidden-md-up" data-toggle="collapse" data-target="#collapse-1">
-							<span class="more"></span>
-						</div>
+			<nav class="navbar navbar-dark">
+				<div class="navbar-left">
+					<a class="navbar-brand" href="/">
+						<div class="logo"></div>
+					</a>
+					<div class="toggle-button dark float-xs-right hidden-md-up" data-toggle="collapse" data-target="#collapse-1">
+						<span class="more"></span>
 					</div>
-					<div class="navbar-right navbar-toggleable-sm collapse" id="collapse-1">
-						<!-- Menu para el perfil de usuario -->
-						<ul class="nav navbar-nav float-md-right">
-							<li class="nav-item dropdown hidden-sm-down">
-								<a href="#" data-toggle="dropdown" aria-expanded="false">
-									<span class="avatar box-32">
-										<img src="img/avatars/1.jpg" alt="">
-									</span>
+				</div>
+				<div class="navbar-right navbar-toggleable-sm collapse" id="collapse-1">
+					<!-- Menu para el perfil de usuario -->
+					<ul class="nav navbar-nav float-md-right">
+						<li class="nav-item dropdown hidden-sm-down">
+							<a href="#" data-toggle="dropdown" aria-expanded="false">
+								<span class="avatar box-32">
+									<img src="img/avatars/1.jpg" alt="">
+								</span>
+							</a>
+							<div class="dropdown-menu dropdown-menu-right animated fadeInUp">
+								<a class="dropdown-item" href="#">
+									<i class="ti-user mr-0-5"></i> Profile
 								</a>
-								<div class="dropdown-menu dropdown-menu-right animated fadeInUp">
-									<a class="dropdown-item" href="#">
-										<i class="ti-user mr-0-5"></i> Profile
-									</a>
-									<a class="dropdown-item" href="#">
-										<i class="ti-settings mr-0-5"></i> Settings
-									</a>
-									<div class="dropdown-divider"></div>
-									<a class="dropdown-item" href="#"><i class="ti-help mr-0-5"></i> Help</a>
-									<a class="dropdown-item" href="#"><i class="ti-power-off mr-0-5"></i> Sign out</a>
-								</div>
-							</li>
-						</ul>
-						<ul class="nav navbar-nav">
-							<li class="nav-item hidden-sm-down">
-								<a class="nav-link toggle-fullscreen" href="#">
-									<i class="ti-fullscreen"></i>
+								<a class="dropdown-item" href="#">
+									<i class="ti-settings mr-0-5"></i> Settings
 								</a>
-							</li>
-						</ul>
-						<!-- Formulario de busqueda -->
-						<div class="header-form float-md-left ml-md-2">
-							<form>
-								<input type="text" class="form-control b-a" placeholder="Search for...">
-								<button type="submit" class="btn bg-white b-a-0">
-									<i class="ti-search"></i>
-								</button>
-							</form>
-						</div>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#"><i class="ti-help mr-0-5"></i> Help</a>
+								<a class="dropdown-item" href="#"><i class="ti-power-off mr-0-5"></i> Sign out</a>
+							</div>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav">
+						<li class="nav-item hidden-sm-down">
+							<a class="nav-link toggle-fullscreen" href="#">
+								<i class="ti-fullscreen"></i>
+							</a>
+						</li>
+					</ul>
+					<!-- Formulario de busqueda -->
+					<div class="header-form float-md-left ml-md-2">
+						<form>
+							<input type="text" class="form-control b-a" placeholder="Search for...">
+							<button type="submit" class="btn bg-white b-a-0">
+								<i class="ti-search"></i>
+							</button>
+						</form>
 					</div>
-				</nav>
-            </div>
+				</div>
+			</nav>
 		</div>
 		
 		<div class="site-content">
