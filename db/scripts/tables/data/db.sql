ab012c8eb1c12085e0a6f2690937f151215f49d1
codeandoclub <codeandoclub@gmail.com>
2020-01-11 23:56:51 -0600

    Se completa información de base de datos.

--- db/scripts/tables/data/db.sql
+++ db/scripts/tables/data/db.sql
@@ -577,3 +577,38 @@ CREATE TABLE IF NOT EXISTS `tb_amparos` (
 
 alter table tb_amparos add requerimientos varchar(250) null;
 alter table tb_amparos add activo bit default 1;
+
+CREATE TABLE `tb_catobservaciones` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(20) NOT NULL,
+	`Activo` BIT(1) NULL DEFAULT b'1',
+	`createAT` TIMESTAMP NULL DEFAULT NULL,
+	`updateAT` TIMESTAMP NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+
+CREATE TABLE `tb_transparencia` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`Folio` VARCHAR(50) NULL DEFAULT NULL,
+	`Expediente` VARCHAR(25) NULL DEFAULT NULL,
+	`Solicitante` VARCHAR(50) NULL DEFAULT NULL,
+	`Recepcion` DATETIME NULL DEFAULT NULL,
+	`Informacion` VARCHAR(50) NULL DEFAULT NULL,
+	`Derivado` VARCHAR(50) NULL DEFAULT NULL,
+	`Canalizacion` VARCHAR(50) NULL DEFAULT NULL,
+	`Respuesta` VARCHAR(100) NULL DEFAULT NULL,
+	`Envio_UT` VARCHAR(50) NULL DEFAULT NULL,
+	`Fecha` DATETIME NULL DEFAULT NULL,
+	`idObservacion` INT(11) NULL DEFAULT NULL,
+	`activo` BIT(1) NULL DEFAULT b'1',
+	`created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	INDEX `idObservación` (`idObservacion`),
+	CONSTRAINT `FK_tb_transparencia_tb_catobservaciones` FOREIGN KEY (`idObservacion`) REFERENCES `tb_catobservaciones` (`id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+
